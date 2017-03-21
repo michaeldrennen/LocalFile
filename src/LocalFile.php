c5c829cd2919d292ebb0eb55800aa5ba645674e6
Michael Drennen <michaeldrennen74@gmail.com>
2017-03-21 07:06:14 -0600

    Created the lineCount function.

---
+++ src/LocalFile.php
@@ -0,0 +1,30 @@
+<?php
+namespace MichaelDrennen;
+use Exception;
+class LocalFile{
+
+    /**
+     * A php function that returns the number of lines in a file on the local file system.
+     * @param string $filePath
+     * @return int
+     * @throws Exception
+     */
+    public static function lineCount(string $filePath): int{
+        
+        $lineCount = 0;
+        $handle = fopen($filePath, "r");
+        if($handle === false){
+            throw new Exception("Unable to open the file at: " . $filePath);
+        }
+        while(!feof($handle)){
+            $line = fgets($handle);
+            if($line === false){
+                throw new Exception("Unable to get a line from the file at: " . $filePath);
+            }
+            $lineCount++;
+        }
+
+        fclose($handle);   
+        return $lineCount;
+    }
+}
---
+++ tests/LocalFileTest.php
@@ -0,0 +1,4 @@
+<?php
+/**
+ * @todo Add tests.
+ */
